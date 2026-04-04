bdca4936d19982f6d56f1837d10d02e6ba0899fe
sbx_user1051 <sbx_user1051@169.254.70.173>
2026-04-04 13:49:43 +0000

    Sat Apr 04 13:49:43 UTC 2026

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -2,5 +2,8 @@
 class SumSolution:
     
     def compute(self, x, y):
-        
+        if not (0 <= x <= 100) and not (0 <= y <= 100):
+            raise ValueError("X and Y must be between 0 and 100")
+        return x + y
+
 
