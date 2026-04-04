6fe42ca9df8b5da659755eed6bc259f9b63a4725
sbx_user1051 <sbx_user1051@169.254.70.173>
2026-04-04 13:52:43 +0000

    Sat Apr 04 13:52:43 UTC 2026

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -2,8 +2,9 @@
 class SumSolution:
     
     def compute(self, x, y):
-        if not (0 <= x <= 100) and not (0 <= y <= 100):
+        if not (0 <= x <= 100) or not (0 <= y <= 100):
             raise ValueError("X and Y must be between 0 and 100")
         return x + y
+
 
 
--- test/solution_tests/SUM/test_sum_solution.py
+++ test/solution_tests/SUM/test_sum_solution.py
@@ -1,6 +1,19 @@
 from solutions.SUM.sum_solution import SumSolution
+import pytest
 
 
 class TestSum():
     def test_sum(self):
         assert SumSolution().compute(1, 2) == 3
+
+    @pytest.mark.parametrize("x, y", [(-1, 2), (101, 2), (1, -1), (1, 101)])
+    def test_sum_out_of_range(self, x, y):
+        with pytest.raises(ValueError):
+            SumSolution().compute(x, y)
+
+
+    @pytest.mark.parametrize("x, y", [(0, 0), (100, 0), (0, 100), (100, 100), (50, 50)])
+    def test_sum_in_range(self, x, y):
+        assert SumSolution().compute(x, y) == x + y
+
+
